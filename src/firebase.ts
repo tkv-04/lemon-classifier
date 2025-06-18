c75e3d0de8925c0339eb573bcd9c2be3d447a031
tkv-04 <thomaskv415@gmail.com>
2025-06-18 15:05:23 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMbCYgiB13yaPIlEIHCUMJ8Ts6DtqbPHI",
-  authDomain: "esp32mini-d487f.firebaseapp.com",
-  projectId: "esp32mini-d487f",
-  storageBucket: "esp32mini-d487f.firebasestorage.app",
-  messagingSenderId: "317415195110",
-  appId: "1:317415195110:web:159cf584e868f9ab5f1627"
+  apiKey: "FIREBASE_API_KEY",
+  authDomain: "FIREBASE_AUTH_DOMAIN",
+  projectId: "FIREBASE_PROJECT_ID",
+  storageBucket: "FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "FIREBASE_MESSAGE_ID",
+  appId: "FIREBASE_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
